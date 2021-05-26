590b6e81832090f2962b009fb1aa141a93d360b5
euleonnnn <leonhung124@gmail.com>
2021-05-26 15:46:58 +0800

    Documentation for models

--- models/Profile.js
+++ models/Profile.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
+
+/**
+ * The ProfileSchema will be a schema which maps to 
+ * our MongoDB collection, where each key in the document
+ * are the features of a profile. 
+ * @param user Contains the user id 
+ * @param faculty Home faculty of user
+ * @param year Year of education eg. Year 1, Year 4
+ * @param interests List of sports user is interested in 
+ * @param bio Short write up for other users to see
+ * @param social Contains the instagram and facebook field 
+ * @param date Date when profile is established
+ */
+
 const ProfileSchema = new mongoose.Schema ({
     user: {
-        type: mongoose.Schema.Types.ObjectId, //profile should be associated with user id of a user
+        type: mongoose.Schema.Types.ObjectId, 
         ref: 'user'
     },
     faculty: {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
+/**
+ * The UserSchema will be a schema which maps to 
+ * our MongoDB collection, where each key in the document
+ * are the features of a user. 
+ * @param name Full name of the user
+ * @param email Full NUS Email address of user, must be unique
+ * @param password Password of user, min 7 characters
+ * @param avatar Link to profile picture
+ * @param date Date user created
+ * @param followers List of users followed by
+ * @param following List of users currently following, also known as friends
+ */
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
