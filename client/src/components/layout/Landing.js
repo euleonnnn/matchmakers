cd6b218bdd1f76915c048fa108c6a8c7864aefdf
euleonnnn <leonhung124@gmail.com>
2021-05-28 16:04:51 +0800

    Redesign landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,24 +17,14 @@ const Landing = ({isAuthenticated}) => {
             <div className="landing-inner">
        
               <div className = "my-3">
-                <h3> Welcome to (Match) Maker. </h3>
-                <p> 
+                <h3 className ="large"> Welcome to (Match) Maker. </h3>
                 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                  deserunt mollit anim id est laborum.
-                </p>
               </div>
-
-
               
               <div className="buttons">
-                <Link to="/register" className="btn btn-primary">Sign Up</Link>
+                <Link to="/register" className="btn btn-primary btn-lg">Sign Up</Link>
                 <>            </>
-                <Link to="/login" className = "btn btn-light">Login</Link>
+                <Link to="/login" className = "btn btn-light btn-lg">Login</Link>
               </div>
             </div>
       </div>
