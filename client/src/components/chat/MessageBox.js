03fa5aeb475d340f6ba46fab6505ce55fcddd65e
seanlaiys <seanlaiyuensheng@gmail.com>
2021-06-10 00:56:10 +0800

    Finished Implementation

--- client/src/components/chat/MessageBox.js
+++ client/src/components/chat/MessageBox.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -6,38 +6,54 @@ import Spinner from '../layout/Spinner';
 import Chat from './Chat';
 import Message from './Message';
 import { getChats } from '../../actions/chat';
-
+import axios from 'axios';
 
 //main page for display of all conversations and messages 
-const MessageBox = ({getChats, auth: { user }, chat}) => {
+const MessageBox = ({getChats, auth: { user }, chat : {chats}}) => {
+    const [currChat, setChat] = useState(null);
+    const [messages, setMessages] = useState([]);
+
     useEffect(() => {
         getChats();
       }, [getChats]);
 
+    useEffect(() => {
+        const getMessages = async () => {
+          try {
+            const res = await axios.get(`/api/message/${currChat._id}`);
+            setMessages(res.data);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        getMessages();
+      }, [currChat]);
+
     return <Fragment>
-        <h1 className = "large text-dark big-header"> My Messages </h1>
         <div className ="row">
         <div className="col-sm-4 col-md-4">
-            <Chat />
-            <Chat />
-            <Chat />
-            <Chat />
+            {chats.map((convo => (
+                <div onClick ={()=> setChat(convo)}>
+                    <Chat convo = {convo}/> 
+                </div>
+            )))}
         </div>
+        {currChat ? 
         <div className="col-sm-8 col-md-8"> 
             <div className="chatbox">
-                < Message />
-                < Message sent ={true}/>
-                < Message />
-                < Message />
-                < Message sent ={true}/>
-                < Message sent ={true}/>
-                < Message sent ={true}/>
+                {messages.map((msg) => (
+                    <div >
+                      <Message message={msg} sent={msg.sender === user._id} />
+                    </div>
+                  ))}
             </div>
             <div className ="input-group my-top">
             <textarea type="text" className ="form-control rounded" placeholder="Type Something" aria-describedby="search-addon" />
             <button type="button" class="btn btn-outline-primary my-right"> <i class="fas fa-paper-plane" /> </button>
             </div>
         </div>
+        : <span>Click on a Profile to Begin Chatting.</span>
+        }
         </div>
     </Fragment> 
 }
