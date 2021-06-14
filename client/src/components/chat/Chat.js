0beb1e8511e521056660541d6e8dd881b854ca08
seanlaiys <seanlaiyuensheng@gmail.com>
2021-06-14 21:31:03 +0800

    Added DP

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,14 +1,26 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import axios from 'axios';
 
 const Chat = ({auth : {user} , chat , loading }) => {
   const [otherUser, setUser] = useState(null);
+  const [friendImg, setImg] = useState(null);
   
   useEffect(() => {
     const friendName = chat.names.find((name)=> name !== user.name);
     setUser(friendName);
+  });
+
+  useEffect(() => async () => {
+    try {
+    const friendId = chat.users.find((u)=> u !== user._id);
+      const res = await axios.get(`/api/profile/user/${friendId}`);
+      const img = res.data.user.avatar;
+      setImg(img);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
     if (loading || otherUser == null) {
@@ -17,6 +29,7 @@ const Chat = ({auth : {user} , chat , loading }) => {
     else {
       return <Fragment>
         <div className="conversation my-top-small">
+          <img className="chatdp" src={friendImg} alt=""/>
           <span className="conversationName">{otherUser}</span>
         </div>
       </Fragment>
