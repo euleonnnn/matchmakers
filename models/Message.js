cef90c56cb2af8faa7e197ed2dcadc7ef20e98c5
seanlaiys <seanlaiyuensheng@gmail.com>
2021-06-16 04:11:42 +0800

    updated model

--- models/Message.js
+++ models/Message.js
@@ -2,10 +2,16 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
+    avatar: {
+        type: String
+    },
     chatID: {
         type: String
     },
     sender:{
+        type: String
+    },
+    name:{
         type: String
     },
     text: {
