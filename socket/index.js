ffdca10777dce09c817a91264f11a7b2f6799d10
euleonnnn <leonhung124@gmail.com>
2021-06-24 21:20:17 +0800

    Concurrent socket running

--- socket/index.js
+++ socket/index.js
@@ -30,10 +30,12 @@ io.on("connection", (socket) => {
   
     socket.on("sendMessage", ({ senderId, receiverId, text }) => {
       const user = getUser(receiverId);
-      io.to(user.socketId).emit("getMessage", {
-        senderId,
-        text,
-      });
+      if (user) {
+        io.to(user.socketId).emit("getMessage", {
+          senderId,
+          text,
+        });
+      }
     });
   
     socket.on("disconnect", () => {
