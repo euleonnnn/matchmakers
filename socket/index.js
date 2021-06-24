5e884072f46fb559cf0168d039500bd063fd205c
seanlaiys <seanlaiyuensheng@gmail.com>
2021-06-24 17:52:54 +0800

    Real Time Chat Working

--- socket/index.js
+++ socket/index.js
@@ -15,16 +15,30 @@ const removeUser = (socketId) => {
     users = users.filter((user) => user.socketId !== socketId);
 };
 
-io.on("connection", (socket) => {
-    console.log("Connected a user")
-    io.on("addUser", userId => {
-        addUser(userId, socket.id);
-        io.emit("getUsers", users);
-    });
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-      });
-});
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+  
 
+io.on("connection", (socket) => {
+    console.log("a user connected.");
+  
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
+    });
+  
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+      const user = getUser(receiverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("a user disconnected!");
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    });
+  });
