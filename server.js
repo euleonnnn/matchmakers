ca04f49d2c08a37fb33e911183eb3d4fda4eb2d4
seanlaiys <seanlaiyuensheng@gmail.com>
2021-06-27 17:26:21 +0800

    Fix Socket

--- server.js
+++ server.js
@@ -48,7 +48,11 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
 
 let users = [];
 
