e4ac76bfc321b23fee1944250b5aad0bf8953004
euleonnnn <leonhung124@gmail.com>
2021-06-28 22:24:59 +0800

    Updated agora token

--- client/src/components/auth/PasswordChange.js
+++ client/src/components/auth/PasswordChange.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { changePassword } from '../../actions/auth';
 import PropTypes from 'prop-types';
--- client/src/components/video/VideoCall.js
+++ client/src/components/video/VideoCall.js
@@ -88,7 +88,7 @@ class Call extends Component {
   joinChannel = () => {
     let me = this;
     client.join(
-      "0061eb6cb9def814e4e96ac6afc003a47b0IADQqOy6pmt2wbjOZfBlMMYq+OZsFma6KCbsYc0ija17cA29DrUAAAAAEACqPfBqitPVYAEAAQCL09Vg",
+      "0061eb6cb9def814e4e96ac6afc003a47b0IAAuSNemgwFj9x/PrFXzYnn/IVnyiJxdeTtMjA0L8Fe1wQ29DrUAAAAAEACqPfBqGC3bYAEAAQAYLdtg",
       me.props.channel,
       USER_ID,
       function (uid) {
