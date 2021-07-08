07b46b6cbef6282e651f3244a497535640b7eaed
euleonnnn <leonhung124@gmail.com>
2021-07-08 22:54:47 +0800

    Search list filtering

--- client/src/components/games/Games.js
+++ client/src/components/games/Games.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState} from 'react'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -9,6 +9,10 @@ import { getCurrentProfile } from '../../actions/profile';
 
 
 const Games = ( {getCurrentProfile, deleteGame, clearGame, getGames, game: {games, game, loading}, auth}) => {
+
+    const [searchterm, setSearch] = useState('')
+    const [formType, setFormType] = useState("sport");
+
 
     // eslint-disable-next-line
     useEffect(() => {
@@ -28,39 +32,129 @@ const Games = ( {getCurrentProfile, deleteGame, clearGame, getGames, game: {game
         return d1.getTime();
     }
 
+    const displayGame = games ? games
+        .filter(game => (convertTime(game.dateTime) > Date.now()))
+        .filter(g => {
+            if (searchterm === ""){
+                return g
+            }
+            if (g.sport.toLowerCase().includes(searchterm.toLowerCase())) {
+                return g
+            }
+        })
+        .map(game =>
+            <div className="card mb-3">
+            <div className="card-body">
+            <h5 className="card-title">{game.sport}</h5>
+            <br></br>
+            <p className="card-text"> <span className='text-primary'> Location: </span> {game.location==="Others"? game.otherLoc : game.location}</p>
+            <p className="card-text"> <span className='text-primary'> Waiting Room: </span> {game.players.length} players out of {game.maxPlayers}</p>
+            <p className="card-text"> <span className='text-primary'> Time: </span> {dateformat(game.dateTime)} </p>
+            <p className="card-text">
+                <br></br>
+                <small className="text-muted"> Host: {game.name} </small>
+                <br></br>
+                <small className="text-muted"> Created on: {dateformat(game.createTime)} </small>
+            </p>
+            {auth.user._id !== game.user ? <Link to={`/games/${game._id}`} className="btn btn-dark join-all"> Enter Room</Link> :
+            <Fragment>
+             <Link to={`/games/${game._id}`} className="btn btn-dark join-all "> Enter Room </Link>
+             <button onClick={()=>deleteGame(game._id)} type ="button" className="btn btn-danger join-all my-right"> Cancel Room </button>
+             </Fragment>
+             }
+             
+            </div>
+        </div>
+    ) : <></>
+
+    const displayLocation = games ? games
+    .filter(game => (convertTime(game.dateTime) > Date.now()))
+    .filter(g => {
+        if (searchterm === ""){
+            return g
+        }
+        if (g.location.toLowerCase().includes(searchterm.toLowerCase())) {
+            return g
+        }
+    })
+    .map(game =>
+        <div className="card mb-3">
+        <div className="card-body">
+        <h5 className="card-title">{game.sport}</h5>
+        <br></br>
+        <p className="card-text"> <span className='text-primary'> Location: </span> {game.location==="Others"? game.otherLoc : game.location}</p>
+        <p className="card-text"> <span className='text-primary'> Waiting Room: </span> {game.players.length} players out of {game.maxPlayers}</p>
+        <p className="card-text"> <span className='text-primary'> Time: </span> {dateformat(game.dateTime)} </p>
+        <p className="card-text">
+            <br></br>
+            <small className="text-muted"> Host: {game.name} </small>
+            <br></br>
+            <small className="text-muted"> Created on: {dateformat(game.createTime)} </small>
+        </p>
+        {auth.user._id !== game.user ? <Link to={`/games/${game._id}`} className="btn btn-dark join-all"> Enter Room</Link> :
+        <Fragment>
+         <Link to={`/games/${game._id}`} className="btn btn-dark join-all "> Enter Room </Link>
+         <button onClick={()=>deleteGame(game._id)} type ="button" className="btn btn-danger join-all my-right"> Cancel Room </button>
+         </Fragment>
+         }
+         
+        </div>
+    </div>
+    ) : <></>
+
+    const displayName = games ? games
+        .filter(game => (convertTime(game.dateTime) > Date.now()))
+        .filter(g => {
+            if (searchterm === ""){
+                return g
+            }
+            if (g.name.toLowerCase().includes(searchterm.toLowerCase())) {
+                return g
+            }
+        })
+        .map(game =>
+            <div className="card mb-3">
+            <div className="card-body">
+            <h5 className="card-title">{game.sport}</h5>
+            <br></br>
+            <p className="card-text"> <span className='text-primary'> Location: </span> {game.location==="Others"? game.otherLoc : game.location}</p>
+            <p className="card-text"> <span className='text-primary'> Waiting Room: </span> {game.players.length} players out of {game.maxPlayers}</p>
+            <p className="card-text"> <span className='text-primary'> Time: </span> {dateformat(game.dateTime)} </p>
+            <p className="card-text">
+                <br></br>
+                <small className="text-muted"> Host: {game.name} </small>
+                <br></br>
+                <small className="text-muted"> Created on: {dateformat(game.createTime)} </small>
+            </p>
+            {auth.user._id !== game.user ? <Link to={`/games/${game._id}`} className="btn btn-dark join-all"> Enter Room</Link> :
+            <Fragment>
+            <Link to={`/games/${game._id}`} className="btn btn-dark join-all "> Enter Room </Link>
+            <button onClick={()=>deleteGame(game._id)} type ="button" className="btn btn-danger join-all my-right"> Cancel Room </button>
+            </Fragment>
+            }
+            
+            </div>
+        </div>
+    ) : <></>
+
     return loading ? <Spinner /> : <Fragment>
           <h1 className = "large big-header text-dark"> Available Rooms </h1>
+            <h5> Search By : </h5>
+                {formType === "sport" ? <button type="button" className="btn btn-primary my-right">Sports</button> :
+                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("sport")}>Sports</button> }
 
-            <div className ="input-group my-3">
-                <input type="search" className ="form-control rounded" placeholder="Search" aria-label="Search"
-                    aria-describedby="search-addon" />
-                <button type="button" class="btn btn-outline-primary">search</button>
-            </div>
+                {formType === "location" ? <button type="button" className="btn btn-primary my-right">Location</button> :
+                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("location")}>Location</button> }
+
+                {formType === "name" ? <button type="button" className="btn btn-primary my-right">Host Name</button> :
+                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("name")}>Host Name</button> }      
+                <input type="search" className ="form-control rounded my-3" placeholder="Search" aria-label="Search"
+                    onChange={event => {setSearch(event.target.value)}}/>
             <Link to="/create-game" className="btn btn-primary btn-block btn-lg my-4"> Create Your Room </Link>
-        {games ? games.map(game => (convertTime(game.dateTime) < Date.now() ? <></> :
-            <div className="card mb-3">
-                <div className="card-body">
-                <h5 className="card-title">{game.sport}</h5>
-                <br></br>
-                <p className="card-text"> <span className='text-primary'> Location: </span> {game.location==="Others"? game.otherLoc : game.location}</p>
-                <p className="card-text"> <span className='text-primary'> Waiting Room: </span> {game.players.length} players out of {game.maxPlayers}</p>
-                <p className="card-text"> <span className='text-primary'> Time: </span> {dateformat(game.dateTime)} </p>
-                <p className="card-text">
-                    <br></br>
-                    <small className="text-muted"> Host: {game.name} </small>
-                    <br></br>
-                    <small className="text-muted"> Created on: {dateformat(game.createTime)} </small>
-                </p>
-                {auth.user._id !== game.user ? <Link to={`/games/${game._id}`} className="btn btn-dark join-all"> Enter Room</Link> :
-                <Fragment>
-                 <Link to={`/games/${game._id}`} className="btn btn-dark join-all "> Enter Room </Link>
-                 <button onClick={()=>deleteGame(game._id)} type ="button" className="btn btn-danger join-all my-right"> Cancel Room </button>
-                 </Fragment>
-                 }
-                 
-                </div>
-            </div>
-        )) : <></>}
+             
+            { formType === "sport" && displayGame }
+            { formType === "location" && displayLocation}
+            { formType === "name" && displayName}
 
     </Fragment>
 }
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
@@ -6,6 +6,8 @@ import ProfileItem from './ProfileItem'
 import { clearProfile, getProfiles } from '../../actions/profile';
 
 const Profiles = ({clearProfile, getProfiles, profile: { profiles, loading }, auth}) => {
+    const [searchterm, setSearch] = useState('')
+
     useEffect(()  => {
         getProfiles();
     }, [getProfiles]);
@@ -25,14 +27,23 @@ const Profiles = ({clearProfile, getProfiles, profile: { profiles, loading }, au
     return <Fragment> 
         { loading || displayAll.length===0 ? <Spinner /> : <Fragment>
             <h1 className = "large text-dark big-header"> Friends in NUS </h1>
-
-            <div className ="input-group my-3">
-                <input type="search" className ="form-control rounded" placeholder="Search" aria-label="Search"
-                    aria-describedby="search-addon" />
-                <button type="button" class="btn btn-outline-primary">search</button>
-            </div>
+            <h5>Search for Friends :</h5>
+            <input type="search" className ="form-control rounded my-3" placeholder="Find Friend" aria-label="Search"
+            onChange={event => {setSearch(event.target.value)}} />
+         
             <div className="profiles">
-                {displayAll}
+                {profiles.filter( p => {
+                    if (searchterm == "") {
+                        return p
+                    } 
+                    if (p.user.name.toLowerCase().includes(searchterm.toLowerCase())) {
+                        return p
+                    }
+                }).map(profile => {
+                    if (profile.user._id !== auth.user._id) {
+                        return <ProfileItem key = {profile._id} profile ={profile} />
+                    }
+                })}
             </div>
 
         </Fragment>}
