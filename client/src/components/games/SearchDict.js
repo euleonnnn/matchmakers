518f27559f1976cb3654dd31024539760dbaee26
seanlaiys <68644109+seanlaiys@users.noreply.github.com>
2021-07-22 01:02:12 +0800

    Update SearchDict.js

--- client/src/components/games/SearchDict.js
+++ client/src/components/games/SearchDict.js
@@ -1,15 +1,55 @@
 export const activities = 
     [
-        "Badminton", "Basketball", "Frisbee", "Running", "Squash", "Swimming", "Squash", 
+        "Badminton", "Basketball", "Frisbee", "Running", "Squash", "Swimming", 
         "Table Tennis", "Tennis", "MLBB", "Mobile Legends Bang Bang", "CODM", "Call of Duty Mobile",
         "PUBG", "Player Unknown's Battleground", "Wild Rift", "League Of Legends", "LoL", "DOTA", 
-        "Poker", "Chess", "Tetris"
+        "Poker", "Chess", "Tetris", "Handball", "Gymming", "Sepak Takraw", "Netball", "Rugby", "Soccer",
+        "Street Soccer", "Volleyball"
     ];
 
 export const locations = 
     [
-        "Online", "Kent Ridge - MPSH 5", "UTown - Sports Hall 1", "Eusoff Hall", 
+        "Online", "Kent Ridge - MPSH 5", "UTown - Sports Hall 1", "Eusoff Hall", "Raffles Hall",
         "NUS Sports & Recreation Centre","Temasek Hall", "Kent Ridge - Multi-Purpose Fields",
-        "Utown - Open Field", "Kent Ridge - Squash Courts", "NUS Sports & Recreation Centre",
-        "Kent Ridge - MPSH 2", "Kent Ridge - Outdoor Tennis Courts"
+        "Utown - Open Field", "Kent Ridge - Squash Courts", "Kent Ridge - MPSH 2", 
+        "Kent Ridge - Outdoor Tennis Courts", "King Edward VII Hall", "Kent Ridge - MPSH3",
+        "NUS Volleyball Courts (Near University Health Centre)", "Utown", "Kent Ridge Hall,",
+        "NUS Netball Courts (Near University Health Centre)", "NUS Field", "UTown - Stephen Riady Centre",
+        "NUS Street Soccer Courts (Near University Health Centre)", "NUS Sepak Takraw Courts (Near University Health Centre)",
+        "NUS Handball Courts (Near University Health Centre)", "NUS Basketball Courts (Near University Health Centre)"
+    ];
+
+export const sports = [
+    { sport: "Badminton", locations:["King Edward VII Hall", "Kent Ridge - MPSH 5", "UTown - Sports Hall 1", "Others"], 
+        maxPlayers:["2","3","4","5","6"] },
+    { sport: "Basketball", locations:["Eusoff Hall", "King Edward VII Hall", "NUS Basketball Courts (Near University Health Centre)", "NUS Sports & Recreation Centre", "Raffles Hall", "Temasek Hall","Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Frisbee", locations:["Kent Ridge - Multi-Purpose Fields", "NUS Field", "Utown - Open Field","Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Gymming", locations:["Eusoff Hall", "Kent Ridge Hall,", "Kent Ridge - MPSH3", "King Edward VII Hall", "NUS Sports & Recreation Centre", "Raffles Hall", "Temasek Hall", "UTown", "Others"], 
+    maxPlayers:["2","3","4","5"] },
+    { sport: "Handball", locations:["Eusoff Hall", "King Edward VII Hall", "Raffles Hall", "Kent Ridge - Squash Courts", "NUS Handball Courts (Near University Health Centre)", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Netball", locations:["Eusoff Hall", "King Edward VII Hall", "NUS Netball Courts (Near University Health Centre)", "Raffles Hall", "Temasek Hall", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Rugby", locations:["Kent Ridge - Multi-Purpose Fields", "NUS Field", "Utown - Open Field","Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Running", locations:["Others"], 
+    maxPlayers:["2","3","4","5"] },
+    { sport: "Sepak Takraw", locations:["Eusoff Hall", "King Edward VII Hall", "Raffles Hall", "Kent Ridge - Squash Courts", "NUS Sepak Takraw Courts (Near University Health Centre)", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Soccer", locations:["NUS Field", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Squash", locations:["Eusoff Hall", "King Edward VII Hall", "Kent Ridge - Squash Courts", "Temasek Hall", "Others"], 
+    maxPlayers:["2","3","4"] },
+    { sport: "Street Soccer", locations:["King Edward VII Hall", "Raffles Hall", "Kent Ridge - Squash Courts", "NUS Street Soccer Courts (Near University Health Centre)", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
+    { sport: "Swimming", locations:["NUS Sports & Recreation Centre", "UTown - Stephen Riady Centre", "Others"], 
+    maxPlayers:["2","3","4","5"] },
+    { sport: "Table Tennis", locations:["Eusoff Hall", "Kent Ridge - MPSH 2", "Others"], 
+    maxPlayers:["2","3","4","5","6"] },
+    { sport: "Tennis", locations:["King Edward VII Hall", "Kent Ridge - Outdoor Tennis Courts", "Others"], 
+    maxPlayers:["2","3","4","5","6"] },
+    { sport: "Volleyball", locations:["Eusoff Hall", "King Edward VII Hall", "NUS Volleyball Courts (Near University Health Centre)", "Raffles Hall", "Temasek Hall", "Others"], 
+    maxPlayers:["4","6","8","9","10"] },
     ];
