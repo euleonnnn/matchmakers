b290bf2e53ef7c2ac7c6a07ddac2c2db07c73246
euleonnnn <leonhung124@gmail.com>
2021-07-23 07:41:01 +0800

    Create Activity UI

--- client/src/components/games/CreateGame.js
+++ client/src/components/games/CreateGame.js
@@ -11,18 +11,19 @@ const CreateGame = () => {
         <Fragment>
             <div className ="container">
             <h1 className="large my-btm"> <i class="fas fa-football-ball"/> {" "}
-                Host Your Room 
+                Host Your Activity
             </h1>
             <h5>Select Category:</h5>
-                {formType === "sport" ? <button type="button" className="btn btn-primary my-right">Sports</button> :
-                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("sport")}>Sports</button> }
+                <div>
+                {formType === "sport" ? <button type="button" className="btn btn-primary my-right-small">Sports</button> :
+                <button type="button" className="btn btn-secondary my-right-small" onClick={() => setFormType("sport")}>Sports</button> }
 
-                {formType === "onlineGame" ? <button type="button" className="btn btn-primary my-right">Online Games</button> :
-                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("onlineGame")}>Online Games</button> }
+                {formType === "onlineGame" ? <button type="button" className="btn btn-primary my-right-small">Online Games</button> :
+                <button type="button" className="btn btn-secondary my-right-small" onClick={() => setFormType("onlineGame")}>Online Games</button> }
 
-                {formType === "study" ? <button type="button" className="btn btn-primary my-right">Study Session</button> :
-                <button type="button" className="btn btn-secondary my-right" onClick={() => setFormType("study")}>Study Session</button> }                 
-
+                {formType === "study" ? <button type="button" className="btn btn-primary">Studying</button> :
+                <button type="button" className="btn btn-secondary" onClick={() => setFormType("study")}>Studying</button> }                 
+                </div>
             {formType === "sport"
                 ? <SportsForm/>
                 : formType ==="onlineGame"
