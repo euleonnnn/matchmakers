455b24a430bec963d7fa623bd22a731188471b18
euleonnnn <leonhung124@gmail.com>
2021-07-27 12:01:47 +0800

    Updated token

--- client/src/components/video/VideoCall.js
+++ client/src/components/video/VideoCall.js
@@ -91,7 +91,7 @@ class Call extends Component {
   joinChannel = () => {
     let me = this;
     client.join(
-      "0061eb6cb9def814e4e96ac6afc003a47b0IACqXmRmUffnN8AoaEuJBQEr1sNGl87jHUlvTxKJcuSP8A29DrUAAAAAEAD7XOPUNTv8YAEAAQA1O/xg",
+      "0061eb6cb9def814e4e96ac6afc003a47b0IAA89W0OOwWku68/dDPFsnQG4VckUHYjhuv2k0MPqNodGw29DrUAAAAAEAD7XOPUmNYAYQEAAQCY1gBh",
       me.props.channel,
       this.props.auth.user.name,
       function (uid) {
